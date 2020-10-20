2807cd6f873fdf4973477721863d41194c43735e
ajk8703 <ajkim8703@gmail.com>
2020-10-19 22:29:08 -0400

    Heuristic #1 Finished

--- lab1_part1/spotlessroomba_heuristics.py
+++ lab1_part1/spotlessroomba_heuristics.py
@@ -1,5 +1,5 @@
-from lab1_part1.spotlessroomba_problem import SpotlessRoombaState
-from os import system
+import sys
+from spotlessroomba_problem import SpotlessRoombaState
 from search_heuristics import *
 from spotlessroomba_problem import *
 
@@ -22,25 +22,28 @@ BOTH
 
 #This firstly finds the hamming distance to the closest spot, doing this garuntees it goes to the closest one which is most effiecient. Afterwards it assumes the distance to each other dirty space is 1 in order to underestimate. It's basically just hamming with a few extra steps.
 def spotlessroomba_first_heuristic(state : SpotlessRoombaState)  -> float:
-    heuri = 0
 
-    lowest = system.maxint
+    if state.is_goal_state():
+        return 0
+
+    heuri = len(state.dirty_locations)
+
+    lowest = sys.maxsize
     for tup in state.dirty_locations:
+        if tup == state.position:
+            heuri-=1
         ham = abs(tup[0] - state.position[0]) + abs(tup[1] - state.position[1])
         if ham < lowest:
             lowest = ham
-    lowest += len(state.dirty_locations)-1
-    heuri += lowest
+    heuri += lowest * .01
 
     return heuri
         
 
-
-
-
 def spotlessroomba_second_heuristic(state : SpotlessRoombaState)  -> float:
     # TODO a nontrivial consistent heuristic
     raise NotImplementedError
+
 
 # This gorgeous beutiful masterpiece of a heuristic is technically nontrivial as it isn't always zero. However it uses the same concept as the zero hueristic, saying we are always 1 away from the goal unless we are the goal state. True beauty. This hueristic is made by Alex and only Alex,
 def silly_heuristic(state:SpotlessRoombaState) ->float:
@@ -48,20 +51,10 @@ def silly_heuristic(state:SpotlessRoombaState) ->float:
         return 0
     return 1
 
-# Make sure to update names below, and add any extra you create.
+
 SPOTLESSROOMBA_HEURISTICS = {"Zero" : zero_heuristic,
                         "Arbitrary": arbitrary_heuristic, 
                         "Custom Heur. 1 (admissible)": spotlessroomba_first_heuristic,
                         "Custom Heur. 2 (consistent)" : spotlessroomba_second_heuristic,
                         "Silly" : silly_heuristic
                         }
-
-
-
-# edit this obvi
-ROOMBA_HEURISTICS = {
-    "Zero" : zero_heuristic, 
-    "Arbitrary": arbitrary_heuristic, 
-    "Manhattan Dist. (one goal)" : roomba_manhattan_onegoal,
-    "Manhattan Dist. (closest)" : roomba_manhattan_multigoal
-    }
